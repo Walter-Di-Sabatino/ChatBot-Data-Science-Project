8235a7a7ff13dc597c55409b4fe92f30e7702729
Walter Di Sabatino <91206724+Walter-Di-Sabatino@users.noreply.github.com>
2024-12-13 15:46:38 +0100

    other one

--- actions/actions.py
+++ actions/actions.py
@@ -12,8 +12,9 @@ from rasa_sdk.events import SlotSet
 import random
 from sqlalchemy import func
 import math
+from fuzzywuzzy import fuzz
 from typing import Text, List, Any, Dict
-
+from rasa_sdk.events import SlotSet, AllSlotsReset
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
@@ -62,6 +63,23 @@ def game_info_response(game, publishers_developers):
     response = f"{game.name} was released on {release_date} by {pub_names}. It costs {game.price} USD and was developed by {dev_names}."
     return response
 
+def fuzzy_finding(check_name, names, threshold=50):
+    # Inizializza variabili per tenere traccia del miglior punteggio e del miglior gioco
+    best_name = None
+    highest_score = 0
+
+    # Applica fuzzywuzzy per trovare la corrispondenza più simile
+    for name in names:
+        # Usa fuzz.ratio per un confronto più completo
+        score = fuzz.ratio(name.name.lower(), check_name.lower())  # Confronto case-insensitive
+        
+        # Se il punteggio è maggiore del punteggio più alto trovato finora e supera la soglia
+        if score > highest_score and score >= threshold:
+            highest_score = score
+            best_name = name
+
+    return best_name
+
 
 class ActionProvideGameInfo(Action):
     def name(self) -> Text:
@@ -71,13 +89,18 @@ class ActionProvideGameInfo(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        game = tracker.get_slot("game")
-        if not game:
+        original_game = tracker.get_slot("game")
+        if not original_game:
             dispatcher.utter_message(text="I need the name of the game to provide details.")
             return []
         
         session = get_session()
-        game = get_game_by_name(session, game)
+
+        # all_games = get_all_games(session)
+        # best_game = fuzzy_finding(original_game, all_games)
+        # game = get_game_by_name(session, best_game.name)
+
+        game = get_game_by_name(session, original_game)
 
         if game:
             publishers_developers = get_developers_and_publishers_by_games(session , game.app_id)
@@ -88,10 +111,91 @@ class ActionProvideGameInfo(Action):
 
             dispatcher.utter_message(image=game.header_image, text=game.short_description)
         else:
-            dispatcher.utter_message(text=f"Sorry, I couldn't retrieve details for the game '{game}'.")
+            dispatcher.utter_message(text=f"Sorry, I couldn't retrieve details for the game '{original_game}'.")
         
         session.close()
         return [SlotSet("game", None)]
+    
+class ActionProvidePublisherGames(Action):
+    def name(self) -> Text:
+        return "action_provide_publisher_games"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        original_publisher = tracker.get_slot("publisher")
+        if not original_publisher:
+            dispatcher.utter_message(text="I need the name of the publisher to provide details.")
+            return []
+        
+        session = get_session()
+
+        games = get_top_games_by_publisher(session, original_publisher)
+
+        if not games:
+            dispatcher.utter_message(text=f"Sorry, I couldn't find any games for the publisher {original_publisher}.")
+        else:
+            dispatcher.utter_message(text=f"Here are 5 of our reccomendations based on the publisher {original_publisher}:")
+            games_ids = [game.app_id for game in games]
+            publishers_developers = get_developers_and_publishers_by_games(session , games_ids)
+            for game in games:
+                response = ""
+                response += game_info_response(game, publishers_developers)
+                response += game.short_description
+                dispatcher.utter_message(image=game.header_image, text=response)
+        
+        session.close()
+        return [SlotSet("publisher", None)]
+
+class ActionProvideGenres(Action):
+    def name(self) -> Text:
+        return "action_provide_genres"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        session = get_session()
+
+        genres = get_top_tags(session)
+        
+        # Crea il messaggio da inviare
+        message = f"I have a total of {len(genres)} genres and subgenres available. These are the 10 most popular:\n\n"
+        
+        for i, (genre, game_count) in enumerate(genres[:10], 1):  # Prendi solo i primi 10
+            message += f"{i}. {genre.name} - {game_count} games in this genre\n"
+        
+        dispatcher.utter_message(message)
+        
+        session.close()
+        return []
+    
+class ActionProvidePublishers(Action):
+    def name(self) -> Text:
+        return "action_provide_publishers"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        session = get_session()
+
+        # Ottieni i top publisher con il conteggio dei giochi
+        publishers = get_top_publishers(session)
+        
+        # Crea il messaggio da inviare
+        message = f"I have {len(publishers)} publishers available. These are the 10 who have produced the most games:\n\n"
+        
+        # Aggiungi i dettagli dei primi 10 publisher in una lista numerata
+        for i, (publisher, game_count) in enumerate(publishers[:10], 1):  # Prendi solo i primi 10
+            message += f"{i}. {publisher.name} - Games produced: {game_count}\n"
+        
+        # Manda il messaggio
+        dispatcher.utter_message(message)
+        
+        session.close()
+        return []
 
 class ActionProvideGenreRecommendation(Action):
     def name(self) -> Text:
@@ -111,6 +215,8 @@ class ActionProvideGenreRecommendation(Action):
         if not games:
             dispatcher.utter_message(text=f"Sorry, I couldn't find any games for the {genre} and {publisher} combination.")
         else:
+            dispatcher.utter_message(text=f"Here are 5 of our reccomendations based on the {genre} and {publisher} combination:")
+
             games_ids = [game.app_id for game in games]
             publishers_developers = get_developers_and_publishers_by_games(session , games_ids)
             for game in games:
@@ -123,7 +229,16 @@ class ActionProvideGenreRecommendation(Action):
         session.close()
         return [SlotSet("genre", None), SlotSet("publisher", None)]
 
-
+class ActionResetSlots(Action):
+    def name(self) -> Text:
+        return "action_reset_slots"
+    
+    def run(self,
+            #slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [AllSlotsReset()]
 
 """class ActionProvideGenreRecommendation(Action):
     def name(self) -> Text:
--- database/db_queries.py
+++ database/db_queries.py
@@ -3,6 +3,8 @@ from database.models import *
 from sqlalchemy import func, case
 from sqlalchemy.orm import joinedload
 
+def get_all_games(session: Session):
+    return session.query(Game).all()
 
 def get_game_by_name(session: Session, game_name: str):
     return session.query(Game).filter(Game.name.ilike(game_name)).first()
@@ -36,6 +38,23 @@ def get_developers_and_publishers_by_games(session: Session, game_ids):
         }
     return result
 
+def get_top_publishers(session: Session):
+    return (
+    session.query(Publisher, func.count(GamePublisher.app_id).label('game_count'))
+    .join(GamePublisher, GamePublisher.publisher_id == Publisher.publisher_id)
+    .group_by(Publisher.publisher_id)
+    .order_by(func.count(GamePublisher.app_id).desc())
+    .all()
+)
+
+def get_top_tags(session: Session):
+    return (
+    session.query(Tag, func.count(GameTag.app_id).label('game_count'))
+    .join(GameTag, GameTag.tag_id == Tag.tag_id)
+    .group_by(Tag.tag_id)
+    .order_by(func.count(GameTag.app_id).desc())
+    .all()
+)
 
 def get_games_by_tag(session: Session, tag_id: int):
     return session.query(Game) \
@@ -77,19 +96,20 @@ def get_top_games_by_publisher_and_tag(session, publisher_name, tag_name, limit=
         .all()
         )
 
+def get_top_games_by_publisher(session, publisher_name, limit=5):
+    score_expr = (
+        (func.coalesce(Game.positive, 0) / func.coalesce(Game.positive + Game.negative, 1))
+        * func.log(func.coalesce(Game.positive - Game.negative, 0) + 1)
+    )
+
+    return (
+        session.query(Game) 
+        .join(Game.publishers) 
+        .filter(Publisher.name.ilike(publisher_name)) 
+        .order_by(score_expr.desc()) 
+        .limit(limit) 
+        .all()
+        )
+
 def get_tag_by_name(session: Session, tag_name: str):
     return session.query(Tag).filter(Tag.name.ilike(tag_name)).first()
-
-def get_top_tags(session: Session, limit: int = 5):
-    subquery = session.query(
-        GameTag.tag_id, 
-        func.count(GameTag.app_id).label('game_count')
-    ).group_by(GameTag.tag_id).subquery()
-
-    return session.query(
-        Tag, 
-        subquery.c.game_count
-    ).join(subquery, Tag.tag_id == subquery.c.tag_id) \
-     .order_by(subquery.c.game_count.desc()) \
-     .limit(limit) \
-     .all()
